201b2571128671f1ae7d9a1258276d53d75c37f9
omar alkhateeb <oalkhateeb1710@gmail.com>
2026-06-03 17:50:53 +0300

    Update JobApplicationHelper.php

--- app/Helpers/JobApplicationHelper.php
+++ app/Helpers/JobApplicationHelper.php
@@ -22,8 +22,8 @@ class JobApplicationHelper
             $view_name = 'emails.job_application';
             $subject = 'Job Applications: New Job Application';
 
-            $email_address = 'oalkhateeb1710@gmail.com';
-            // $email_address = 'o.bsharat@beveconsult.com';
+            // $email_address = 'oalkhateeb1710@gmail.com';
+            $email_address = 'o.bsharat@beveconsult.com';
             // $email_address = $email_ditals['mail_to'];
 
             $data = [
@@ -44,51 +44,6 @@ class JobApplicationHelper
             throw $exception;
         }
 
-    }
-
-    public function sendOwnerNotify($email_ditals)
-    {
-
-        $view_name = 'emails.approver_email_notification';
-        $subject = 'e-Qiwa-EOS: New Qiwa EOS Request Alert';
-
-        // Split the name by spaces
-        $nameParts = explode(' ', $email_ditals['next_approver_name']);
-
-        // Get the first name
-        $email_ditals['next_approver_name'] = $nameParts[0];
-
-        $email_address = 'oalkhateeb1710@gmail.com';
-        // $email_address = $email_ditals['mail_to'];
-
-        $data = [
-            'header' => "Ajmi Company",
-            'title' => "New Qiwa EOS Request Alert",
-            'message' => $email_ditals['message'] ?? null,
-            'qiwa_eos_request_id' => $email_ditals['qiwa_eos_request_id'] ?? null,
-            'next_approver_number' => $email_ditals['next_approver_number'] ?? null,
-            'next_approver_name' => $email_ditals['next_approver_name'] ?? null,
-            'created_by' => $email_ditals['created_by'] ?? null,
-            'employee_number' => $email_ditals['employee_number'] ?? null,
-            'employee_name' => $email_ditals['employee_name'] ?? null,
-            'cost_center' => $email_ditals['cost_center'] ?? null,
-            'notes' => $email_ditals['notes'] ?? null,
-            'job' => $email_ditals['job'] ?? null,
-            'nationality' => $email_ditals['nationality'] ?? null,
-            'iqama_number' => $email_ditals['iqama_number'] ?? null,
-            'e_qiwa_number' => $email_ditals['e_qiwa_number'] ?? null,
-            'qiwa_request_date' => $email_ditals['qiwa_request_date'] ?? null,
-            'type_name' => $email_ditals['type_name'] ?? null,
-            'login_user' => $email_ditals['login_user'] ?? null,
-        ];
-
-        if (!empty($email_address)) {
-            // $emailSent = Mail::to($email_address)->send(new QiwaEosNotificationMail($data, $view_name, $subject));
-            // return $emailSent;
-        } else {
-            Log::info('Email not sent: email_to is empty.', $email_ditals);
-            return false;
-        }
     }
 
 
